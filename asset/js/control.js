7d9b1d1c03fe225c03f08472d89070f1da9ab6d2
han12332123 <gmldid4696@naver.com>
2024-10-19 04:11:57 +0900

    Update control.js

--- asset/js/control.js
+++ asset/js/control.js
@@ -8,7 +8,7 @@ let earthOpacityCon = 1; //지구본 녹화 값 0~1
 let tempNow = 40; //시작 온도값
 let tempCon = 14; //목표 온도값 백분율
 let limitDate = new Date(); //종료일 기본값 오늘날짜
-let startDate = '2023-1-11' //시작일
+let startDate = '2024-1-1' //시작일
 let notifiTrigger = 800; //알람 지속 시간
 
 
@@ -35,7 +35,7 @@ let notifArry = [ //프로젝트명 기술
 
 //https://ohmyluck.com/ko/random-number/ 난수생성사이트
 let notifTriggerNum = [ //알림 트리거 설정
-"2023-01-1",
+"2023-01-01",
 "2023-02-01",
 "2023-03-01",
 "2024-04-01",
