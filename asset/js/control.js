8e7be02b534bec105180615a7ca5e1eeb6195c00
han12332123 <gmldid4696@naver.com>
2024-10-22 23:42:21 +0900

    Update control.js

--- asset/js/control.js
+++ asset/js/control.js
@@ -6,7 +6,7 @@ let pauseTimeCon = 2000; // 전체 큐 길이값
 let intervalTimeCon = 20; //틱 시간값 1000ms = 1s
 let earthOpacityCon = 0.1; //지구본 녹화 값 0~1
 let tempNow = 40; //시작 온도값
-let tempCon = 39.7; //목표 온도값 백분율
+let tempCon = 39.5; //목표 온도값 백분율
 let limitDate = new Date(); //종료일 기본값 오늘날짜
 let startDate = '2024-1-1' //시작일
 let notifiTrigger = 800; //알람 지속 시간
@@ -44,9 +44,10 @@ let notifTriggerNum = [ //알림 트리거 설정
 
 export let rankNameList = [
     {name:"대학4-H 서울아카데미",temp:0.1},
-    {name:"전북대학교4-H회",temp:0.1},
+    {name:"전북대학교4-H회",temp:0.2},
     {name:"영남대학교4-H회",temp:0.1},
-]    
+    {name:"한국농수산대학교4-H회",temp:0.1},
+]
 
 rankNameList.sort((a, b) => b.temp - a.temp);
 
