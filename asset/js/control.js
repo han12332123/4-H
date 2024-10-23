fe2f5c649dfb883f286fd88d389b31361464cd12
han12332123 <gmldid4696@naver.com>
2024-10-23 10:32:59 +0900

    Update control.js

--- asset/js/control.js
+++ asset/js/control.js
@@ -6,7 +6,7 @@ let pauseTimeCon = 2000; // 전체 큐 길이값
 let intervalTimeCon = 20; //틱 시간값 1000ms = 1s
 let earthOpacityCon = 0.1; //지구본 녹화 값 0~1
 let tempNow = 40; //시작 온도값
-let tempCon = 39.5; //목표 온도값 백분율
+let tempCon = 39.4; //목표 온도값 백분율
 let limitDate = new Date(); //종료일 기본값 오늘날짜
 let startDate = '2024-1-1' //시작일
 let notifiTrigger = 800; //알람 지속 시간
@@ -28,6 +28,7 @@ let notifArry = [ //프로젝트명 기술
     '영남대학교4-H회 텀블러 사용-0.1℃',
     '한국농수산대학교4-H회 텀블러 사용-0.1℃',
     '잔북대학교4-H회 에코백 사용-0.1℃',
+    '연암대학교4-H회 텀블러 사용-0.1℃',
 ]
 
 
@@ -39,7 +40,7 @@ let notifTriggerNum = [ //알림 트리거 설정
 "2024-01-20",
 "2024-01-30",
 "2024-02-10",
-
+"2024-02-20",
 ]
 
 
@@ -52,6 +53,7 @@ export let rankNameList = [
     {name:"전북대학교4-H회",temp:0.2},
     {name:"영남대학교4-H회",temp:0.1},
     {name:"한국농수산대학교4-H회",temp:0.1},
+    {name:"연암대학교4-H회",temp:0.1},
 ]
 
 rankNameList.sort((a, b) => b.temp - a.temp);
