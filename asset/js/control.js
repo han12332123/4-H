94f5c98bde0a4f57e116d1015028fa9c03972785
han12332123 <gmldid4696@naver.com>
2024-11-05 21:19:37 +0900

    Update control.js

--- asset/js/control.js
+++ asset/js/control.js
@@ -6,7 +6,7 @@ let pauseTimeCon = 2000; // 전체 큐 길이값
 let intervalTimeCon = 20; //틱 시간값 1000ms = 1s
 let earthOpacityCon = 0.3; //지구본 녹화 값 0~1
 let tempNow = 40; //시작 온도값
-let tempCon = 34.5; //목표 온도값 백분율
+let tempCon = 34.2; //목표 온도값 백분율
 let limitDate = new Date(); //종료일 기본값 오늘날짜
 let startDate = '2024-1-1' //시작일
 let notifiTrigger = 800; //알람 지속 시간
@@ -38,6 +38,8 @@ let notifArry = [ //프로젝트명 기술
     '전북대학교4-H회 대중교통 이용-0.1℃',
     '전북대학교4-H회 대중교통 이용-0.1℃',
     '전북대학교4-H회 친환경제품 이용-0.1℃',
+    '전북대학교4-H회 텀블러 이용-0.1℃',
+    '전북대학교4-H회 전기자전거 이용-0.1℃'
     
 ]
 
@@ -60,7 +62,8 @@ let notifTriggerNum = [ //알림 트리거 설정
 "2024-04-30",
 "2024-05-10",
 "2024-05-20",
-
+"2024-05-30",
+"2024-06-10",
 
 ]
 
@@ -71,7 +74,7 @@ let notifTriggerNum = [ //알림 트리거 설정
 
 export let rankNameList = [
     {name:"대학4-H 서울아카데미",temp:0.1},
-    {name:"전북대학교4-H회",temp:0.6},
+    {name:"전북대학교4-H회",temp:0.9},
     {name:"영남대학교4-H회",temp:0.1},
     {name:"한국농수산대학교4-H회",temp:3.5},
     {name:"연암대학교4-H회",temp:0.2},
