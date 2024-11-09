d5f54cf10973c85a207f9345e5d5a2cc56e7a954
han12332123 <gmldid4696@naver.com>
2024-11-09 21:00:23 +0900

    Update control.js

--- asset/js/control.js
+++ asset/js/control.js
@@ -6,7 +6,7 @@ let pauseTimeCon = 2000; // 전체 큐 길이값
 let intervalTimeCon = 20; //틱 시간값 1000ms = 1s
 let earthOpacityCon = 0.4; //지구본 녹화 값 0~1
 let tempNow = 40; //시작 온도값
-let tempCon = 31.7; //목표 온도값 백분율
+let tempCon = 31.6; //목표 온도값 백분율
 let limitDate = new Date(); //종료일 기본값 오늘날짜
 let startDate = '2024-1-1' //시작일
 let notifiTrigger = 800; //알람 지속 시간
@@ -44,6 +44,7 @@ let notifArry = [ //프로젝트명 기술
     '전북대학교4-H회 대중교통 이용-0.1℃'
     '한경대학교4-H회 플로깅 활동-0.8℃',
     '전북대학교4-H회 대중교통 이용-0.5℃'
+    '전북대학교4-H회 텀블러 사용-0.1℃',
 ]
 
 
@@ -71,6 +72,7 @@ let notifTriggerNum = [ //알림 트리거 설정
 "2024-06-30",
 "2024-07-10",
 "2024-07-20",
+"2024-07-30",
 ]
 
 
@@ -80,12 +82,12 @@ let notifTriggerNum = [ //알림 트리거 설정
 
 export let rankNameList = [
     {name:"대학4-H 서울아카데미",temp:0.1},
-    {name:"전북대학교4-H회",temp:2.6},
+    {name:"전북대학교4-H회",temp:2.7},
     {name:"영남대학교4-H회",temp:0.1},
     {name:"한국농수산대학교4-H회",temp:3.5},
     {name:"연암대학교4-H회",temp:0.2},
     {name:"경북대학교4-H회(상주캠)",temp:1.0},
-    {name:"한경댜학교4-H회",temp:0.8},
+    {name:"한경대학교4-H회",temp:0.8},
 ]
 
 rankNameList.sort((a, b) => b.temp - a.temp);
